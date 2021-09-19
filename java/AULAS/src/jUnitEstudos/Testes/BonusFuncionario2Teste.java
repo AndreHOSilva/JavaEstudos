30f17b315b6f9d751510ef40884688a6982897e7
andre.o.silva <andre.o.silva@ericssoninovacao.com.br>
2021-09-19 15:38:58 -0300

    Arrumando estudos no Junit

--- java/AULAS/src/jUnitEstudos/Testes/BonusFuncionario2Teste.java
+++ java/AULAS/src/jUnitEstudos/Testes/BonusFuncionario2Teste.java
@@ -12,34 +12,33 @@ import java.time.LocalDate;
 
 public class BonusFuncionario2Teste {
 
-    private Funcionario2 funcionario2 ;
-    private BonusFuncionario bonusFuncionario ;
-    private BigDecimal calBonus ;
 
+
+    /*
     @BeforeEach
     protected void Inicializar (){
+
+
+    }
+    @Test
+    public void DeveriaDevolverErroDeCompilacao(){
+       // Inicializar();
+
+        bonusFuncionario.calcularBonus(funcionario2) ;
         funcionario2= new Funcionario2("Teste", LocalDate.now(),new BigDecimal(25000)) ;
 
 
-    }
-
-    @Test
-    public void DeveriaDevolverErroDeCompilacao(){
-        Inicializar();
-
-        //bonusFuncionario.calcularBonus(funcionario2) ;
-
         Assert.assertThrows(IllegalArgumentException.class,() ->bonusFuncionario.calcularBonus(funcionario2));
-
-
-
-
     }
+    */
+
+
+
     @Test
     public void DeveriaDevolverValorDoBonusDoFuncionario(){
-        Inicializar();
-
-        //bonusFuncionario.calcularBonus(funcionario2) ;
+        //Inicializar();
+        Funcionario2 funcionario2 = new Funcionario2("Andre",LocalDate.now(),new BigDecimal(10000.00)) ;
+        BonusFuncionario bonusFuncionario = new BonusFuncionario();
 
         Assert.assertEquals(new BigDecimal("1000.00"), bonusFuncionario.calcularBonus(funcionario2));
 
